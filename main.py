c33c9a87ee631179048764fbb3b134a907bbc5b3
evannol <idcwhatnolol@gmail.com>
2023-11-16 12:34:42 -0500

    make file into list

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-songs = open("songs.dat").readlines()
+try:
+    file = open("songs.dat")
+except:
+    print("no songs.dat file found")
 
+songs = file.read().splitlines()
+
+for song in range(len(songs)):
+    songs[song] = songs[song].split()
+
+print(songs)
